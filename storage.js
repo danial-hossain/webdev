8f9908b028d9e70ed6c8d1dda62dd3325fc448de
edunectpulse <edunectpulse@gmail.com>
2025-06-27 01:52:28 +0600

    Pulse created storage .js

---
+++ storage.js
@@ -0,0 +1,9 @@
+// storage.js
+function saveToStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function loadFromStorage(key) {
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : null;
+}
